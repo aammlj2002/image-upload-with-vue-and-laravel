ddc3a7a15d3e41dec5b9820be11514a38fb90581
Leo <aammlj2002@gmail.com>
2022-03-01 13:57:30 +0630

    added filepond

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,32 @@
 <template>
-    <h1 class="text-xl text-center">{{message}}</h1>
+    <file-pond
+        class="mt-5"
+        name="image"
+        ref="pond"
+        label-idle="click to choose iamge or drage here..."
+        server="upload"
+        @init="filepondInitialized"
+        accepted-file-types="image/*"
+    />
 </template>
 
 <script>
+import "filepond/dist/filepond.min.css"
+import VueFilePond from 'vue-filepond';
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+const FilePond = VueFilePond(FilePondPluginFileValidateType);
 export default {
+    components: {
+        FilePond
+    },
     data() {
         return {
-            message: "hello, world!"
+        }
+    },
+    methods: {
+        filepondInitialized() {
+            console.log("filepond");
+            console.log("filepond Object", this.$refs.pond)
         }
     }
 }
